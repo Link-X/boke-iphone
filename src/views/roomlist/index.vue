79c7a381c51905d90fe1279382b0262a90110dfd
link <link@linkdeMacBook-Pro.local>
2018-03-31 18:52:30 +0800

    2

--- src/views/roomlist/index.vue
+++ src/views/roomlist/index.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="room-list">
-
+    <Scroll class="scroll-item"
+      :data='groupList'>
+      <ul class="group-list_ul">
+        <li v-for="item in groupList"
+          :key="item.friendId"
+          @click="friendMess(item)">
+          <div class="ul-li_img">
+            <img :src="item.userImg"
+              alt="">
+          </div>
+          <div class="ul-li_text">
+            <div class="text-name">{{item.friendUserName}}</div>
+            <div class="text-signature">{{item.signature}}</div>
+          </div>
+        </li>
+      </ul>
+      <div v-if="!groupList.length"
+        class="friend-list_null">
+        没有该群号，快去看看吧
+      </div>
+    </Scroll>
   </div>
 </template>
 
 <script>
+import Scroll from '@/components/scroll.vue'
 export default {
-
+  data () {
+    return {
+      groupList: []
+    }
+  },
+  components: {
+    Scroll
+  }
 }
 </script>
 
-<style>
-
+<style lang='less'>
+.group-list_ul {
+  position: relative;
+  padding: 50px 0 55px 0;
+}
+.friend-list_null {
+  text-align: center;
+}
 </style>
